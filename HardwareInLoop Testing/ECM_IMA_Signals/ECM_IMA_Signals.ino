83d87310cb111bf55a9bd9cd7892104c190cc11a
John Sullivan <buhtig.elt@xoxy.net>
2022-08-10 20:39:33 -0400

    Update ECM_IMA_Signals.ino

--- HardwareInLoop Testing/ECM_IMA_Signals/ECM_IMA_Signals.ino
+++ HardwareInLoop Testing/ECM_IMA_Signals/ECM_IMA_Signals.ino
@@ -23,7 +23,7 @@ void setup()
   TCCR1A = 0b00000011; // 10bit
   TCCR1B = 0b00001010; // x8 fast pwm
 
-  // Pins D3 and D11 - 31.4 kHz
+  // Pins D3 and D11 - 31.4 kHz (OEM is 20 kHz, but this is close enough)
   TCCR2B = 0b00000001; // x1 8bit
   TCCR2A = 0b00000001; // phase correct
 }
