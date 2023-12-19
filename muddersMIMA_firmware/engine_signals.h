8d8cacb831535a1b62334b0d903e94a04742f212
Martin Kennedy <hurricos@gmail.com>
2023-12-19 14:35:50 -0500

    feat: speed: Measure vehicle speed using pin-change IRQ on VSS
    This commit implements vehicle speed measurement using a pin-change
    interrupt on the vehicle speed sensor (VSS) line connected to pin D7
    
    The VSS emits ~4250 pulses per mile[^1] under normal conditions. As
    with NEP, attach a pin-change interrupt; if when the interrupt
    activates, the pin is high, recalculate the vehicle speed by checking
    the time since the last revolution.
    
    Also, reflect the new measurement details in
    `debugUSB_printOEMsignals` (the OEM signal echoing mode).
    
    Notes:
    
    - A more precise value can be derived from the Honda DTC manual [^2]:
    
      > The vehicle speed sensor (VSS) is attached to the transmission
      > housing. The VSS outputs a pulsing signal for every revolu- tion
      > of the differential gear via the speedometer gear.
    
    [^1]:
    https://www.insightcentral.net/threads/vss-vehicle-speed-sensor-output.13597/post-191883
    describes it initially as 4550 pulses, but some experimentation and
    comparison to a stock speedo led to the 4250 number now used.
    
    [^2]: https://downloads.laboratoryb.org/insight/other_manuals/HONDA_DTC.pdf#page=106
    
    Signed-off-by: Martin Kennedy <hurricos@gmail.com>

--- muddersMIMA_firmware/engine_signals.h
+++ muddersMIMA_firmware/engine_signals.h
@@ -4,6 +4,7 @@
 #ifndef engine_signals_h
 	#define engine_signals_h
 
+	#define ONE_HOUR_IN_MICROSECONDS 3600E6
 	#define ONE_MINUTE_IN_MICROSECONDS 60E6
 	#define NUM_ENGINE_REVOLUTIONS_PER_CYCLE 2
 	#define NUM_TACHOMETER_PULSES_PER_CYCLE 3
@@ -14,4 +15,5 @@
 
 	uint16_t engineSignals_getLatestRPM(void);
 
+	uint16_t engineSignals_getLatestVehicleMPH(void);
 #endif
